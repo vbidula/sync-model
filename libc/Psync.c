520822d01a5c47f6a73b05f1ac686489b3677259
ddimos <43299351+ddimos@users.noreply.github.com>
2019-08-08 16:56:14 +0300

    Update Psync.c
    Improved performance

--- libc/Psync.c
+++ libc/Psync.c
@@ -1,18 +1,21 @@
 #include "Psync.h"
 
-double pi = 3.141592653589793;
-double e = 4.80320451e-10;
-double c = 29979245800.0;
-double m_e = 9.10938356e-28;
-double h = 6.626e-27;
+const double pi = 3.141592653589793;
+const double e = 4.80320451e-10;
+const double c = 29979245800.0;
+const double m_e = 9.10938356e-28;
+const double h = 6.626e-27;
 
 
-double Psync(double nu, double gamma, double B){
-        double x = nu / ((3 * B * e) / (4 * pi * m_e * c) * gamma * gamma);
+double Psync(const double nu, const double gamma, const double B){
+        double x = nu / ((3.0*B*e) / (4.0*pi*m_e*c) * gamma*gamma);
+        double powX_2over3 = pow(x, 2.0/3.0);
+        double powX_4over3 = pow(x, 4.0/3.0);
+        
         return(
-                1.808 * pow(x, 1 / 3) / pow(1 + 3.4*pow(x,2/3),1/2)
-                * (1 + 2.21 * pow(x,2 / 3) + 0.347 * pow(x,4 / 3))
-                / (1 + 1.353 * pow(x,2 / 3) + 0.217 * pow(x,4 / 3))
+                1.808*pow(x, 1.0/3.0) / pow(1.0 + 3.4*powX_2over3,1/2)
+                * (1.0 + 2.21*powX_2over3 + 0.347*powX_4over3)
+                / (1.0 + 1.353*powX_2over3 + 0.217*powX_4over3)
                 * exp(-x)
               );
 }
