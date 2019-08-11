ef887c1f80a4b3efd89ce3c1bb8cb5caba5b0bc3
lap1dem <vadym.bidula@gmail.com>
2019-08-11 08:16:05 +0300

    Bugs fixed

--- libc/Psync.c
+++ libc/Psync.c
@@ -7,26 +7,15 @@ const double m_e = 9.10938356e-28;
 const double h = 6.626e-27;
 
 
-<<<<<<< HEAD
-double Psync(double nu, double gamma, double B){
-        double x = nu / ((3 * B * e) / (4 * pi * m_e * c) * gamma * gamma);
-        double pow23 = pow(x,2 / 3);
-        double pow43 = pow(x,4 / 3);
-        return(
-                1.808 * pow(x, 1 / 3) / pow(1 + 3.4*pow23,1/2)
-                * (1 + 2.21 * pow23 + 0.347 * pow23)
-                / (1 + 1.353 * pow23 + 0.217 * pow43)
-=======
 double Psync(const double nu, const double gamma, const double B){
         double x = nu / ((3.0*B*e) / (4.0*pi*m_e*c) * gamma*gamma);
         double powX_2over3 = pow(x, 2.0/3.0);
         double powX_4over3 = pow(x, 4.0/3.0);
-        
+
         return(
-                1.808*pow(x, 1.0/3.0) / pow(1.0 + 3.4*powX_2over3,1/2)
+                1.808*pow(x, 1.0/3.0) / pow(1.0 + 3.4*powX_2over3,1.0/2.0)
                 * (1.0 + 2.21*powX_2over3 + 0.347*powX_4over3)
                 / (1.0 + 1.353*powX_2over3 + 0.217*powX_4over3)
->>>>>>> gh/master
                 * exp(-x)
               );
 }
